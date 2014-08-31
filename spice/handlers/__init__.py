b144f9d111a07f8fa24faf18ca767d9965e31e24
JD Cantrell <jcantrell@trulia.com>
2014-08-31 04:42:39 -0700

    Fix default handler

--- spice/handlers/__init__.py
+++ spice/handlers/__init__.py
@@ -12,7 +12,7 @@ def get_handler(filetype):
   for h in handler_classes:
     if filetype in h.extensions:
       return h
-  return Handler
+  return DefaultHandler
 
 def get_handler_instance(record):
   handler_data = get_handler(record.filetype)
