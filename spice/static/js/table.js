431f6ae1a41596f62305f8be0b632d3c4f16801a
JD Cantrell <jd@goodrobot.net>
2019-05-29 10:28:08 -0700

    Fix table view upload and js

--- spice/static/js/table.js
+++ spice/static/js/table.js
@@ -1,61 +1,21 @@
-/* globals bootstrap, File */
-var FileModel = Backbone.Model.extend({
+/* globals enableUploads, bootstrap, Backbone, _, $ */
+const FileModel = Backbone.Model.extend({
   urlRoot: 'file',
-  upload: function () {
-    var self = this;
-    //using XHR directly because jquery does not expose the upload
-    //property - works in chrome and FF (IE shouldn't get this code)
-    var xhr = new XMLHttpRequest();
-
-    xhr.upload.addEventListener("progress", function (e) {
-      if (e.lengthComputable) {
-        var percentage = Math.round((e.loaded * 100) / e.total);
-        self.set({
-          'status': 'uploading',
-          'progress': percentage,
-          'loaded': e.loaded,
-          'total': e.total
-        });
-      }
-    }, false);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if ((xhr.status >= 200 && xhr.status <= 200) || xhr.status === 304) {
-          if (xhr.responseText !== "") {
-            var item = $.parseJSON(xhr.responseText);
-            self.set(item);
-          }
-        }
-      }
-    };
-
-    var data = new FormData();
-    data.append('file', self.get('file'));
-    data.append('access', self.get('access'));
-    data.append('json', true);
-
-    xhr.open("POST", this.url());
-    xhr.send(data);
-  }
 });
 
-var FileView = Backbone.View.extend({
+const FileView = Backbone.View.extend({
   tagName: 'tr',
   className: 'hover',
-  uploadTemplate: _.template($('#uploadTemplate').html()),
   template: _.template($('#rowTemplate').html()),
 
-  events: function () {
-    this.listenTo(this.model, 'change:progress', this.render);
-    this.listenTo(this.model, 'change:key', this.render);
+  events: function eventsInit() {
     return {
       'click .access': '_access',
-      'click .remove': '_remove'
+      'click .remove': '_remove',
     };
   },
 
-  _remove: function (event) {
+  _remove: function fileRemove(event) {
     event.preventDefault();
     event.stopPropagation();
 
@@ -63,151 +23,66 @@ var FileView = Backbone.View.extend({
     this.remove();
   },
 
-  _access: function () {
+  _access: function fileAccess() {
     if (this.model.get('access') === 'public') {
       this.model.set('access', 'limited');
-    }
-    else if (this.model.get('access') === 'private') {
+    } else if (this.model.get('access') === 'private') {
       this.model.set('access', 'public');
-    }
-    else {
+    } else {
       this.model.set('access', 'private');
     }
     this.model.save();
     this.render();
   },
 
-  render: function () {
-    if (this.model.get('key')) {
-      this.$el.html(this.template(this.model.toJSON()));
-    }
-    else {
-      this.$el.html(this.uploadTemplate(this.model.toJSON()));
-    }
-  }
+  render: function fileRender() {
+    console.log('render', this.model.toJSON());
+    this.$el.html(this.template(this.model.toJSON()));
+  },
 });
 
-$(document).ready(function () {
-  var access = 'limited';
-  $('#access_controls button').click(function () {
-    var $el = $(this);
-    access = $el.val();
-    $el.parent().children().addClass('button-lowlight');
-    $el.removeClass('button-lowlight');
-  });
-  //file api code
-  if (typeof FileReader === "function") {
-
-    var checkInput = function () {
-      var pasteCatcher = $('#paste_box');
-      // Store the pasted content in a variable
-      var child = pasteCatcher.children().get(0);
-
-      // Clear the inner html to make sure we're always
-      // getting the latest inserted content
-      pasteCatcher.html('');
-
-      if (child) {
-        // If the user pastes an image, the src attribute
-        // will represent the image as a base64 encoded string.
-        if (child.tagName === "IMG") {
-          createImage(child.src);
-        }
-      }
-    };
-
-    var getFileName = function (type)
-    {
-      var fileTypes = {
-        'image/png': 'png',
-        'image/jpeg': 'jpg',
-        'image/gif': 'gif'
-      };
-      return 'Image ' + new Date() + '.' + fileTypes[type];
-    };
-
-    /* Creates a new image from a given source */
-    var createImage = function(dataURI) {
-      var binary = atob(dataURI.split(',')[1]);
-      var mimeType = dataURI.split(',')[0].split(':')[1].split(';')[0];
-      var array = [];
-      for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i));
-      }
-
-      var blob = new Blob([new Uint8Array(array)], {type: mimeType});
-      var file = new File([blob], getFileName(mimeType));
-      addFile(file);
-    };
-
-    var addFile = function (file)
-    {
-      var model = new FileModel({
-        name: file.name,
-        access: access,
-        progress: 0,
-        status: 'queued',
-        file: file
-      });
-
-      var view = new FileView({model: model});
-      $('#files').prepend(view.el);
-      $('#empty').remove();
-      view.render();
-
-      model.upload();
-
-    };
-
-    $('#paste_box').bind('paste', function (event) {
-      var items = event.originalEvent.clipboardData.items;
-      if (items) {
-        // Loop through all items, looking for any kind of image
-        for (var i = 0; i < items.length; i++) {
-          if (items[i].type.indexOf("image") !== -1) {
-            // TODO: test in chrome
-            var blob = items[i].getAsFile();
-            addFile(blob);
-          }
-        }
-      }
-      else {
-        // If we can't handle clipboard data directly (Firefox),
-        // we need to read what was pasted from the contenteditable element
-        // This is a cheap trick to make sure we read the data
-        // AFTER it has been inserted.
-        $(this).html('');
-        setTimeout(checkInput, 1);
-      }
-    });
-
-    $('body').bind({
-      click: function () { $('#paste_box').focus(); },
-      dragover: function (event) { event.preventDefault(); },
-      dragleave: function (event) { event.preventDefault(); },
-      drop: function (event) {
-        event.preventDefault();
-
-        var originalEvent = event.originalEvent;
-        var files = (originalEvent.files || originalEvent.dataTransfer.files);
-        for (var i = 0; i < files.length; i += 1) {
-          //queue objects to be uploaded
-          var file = files[i];
-          addFile(file);
-        }
-      }
-    });
-
-    //focus on paste catcher
-    $('#paste_box').focus();
-  }
-
-  //initialize existing files
-  _.each(bootstrap, function (file) {
-    var model = new FileModel(file);
-    var view = new FileView({
+$(document).ready(() => {
+  // initialize existing files
+  _.each(bootstrap, file => {
+    const model = new FileModel(file);
+    const view = new FileView({
       model: model,
-      el: $('#key_' + model.get('key')).get(0)
+      el: $(`#key_${model.get('key')}`).get(0),
     });
   });
+});
+
+const addFile = function(file) {
+  console.log(file);
+  const model = new FileModel({ ...file });
+
+  const view = new FileView({ model: model });
+  $('#files').prepend(view.el);
+  view.render();
+};
+
+// const removeItem = (id) => {
+//   fetch(`/file/${id}`, { method: 'DELETE' }).then(response => {
+//     const el = document.getElementById(`photo_card_${id}`);
+//     el.classList.add('photo-card-exit');
+//     setTimeout(() => { el.remove() }, 200);
+//   });
+// };
+//
+// const setMode = (selectEl, id) => {
+//   const newAccess = selectEl.value;
+//   selectEl.previousElementSibling.innerHTML = newAccess;
+//   fetch(`/file/${id}`, {
+//     method: 'PUT',
+//     cache: 'no-cache',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ access: newAccess })
+//
+//   });
+// };
+
+document.addEventListener('DOMContentLoaded', () => {
+  enableUploads(addFile);
 });
--- spice/static/js/upload.js
+++ spice/static/js/upload.js
@@ -1,3 +1,4 @@
+/* exported enableUploads */
 const enableUploads = (onUploadComplete = () => {}) => {
   const uploadFile = (fileData, access, onProgress) => {
     const uploadPromise = new Promise((resolve, reject) => {
@@ -5,16 +6,16 @@ const enableUploads = (onUploadComplete = () => {}) => {
 
       xhr.upload.addEventListener(
         'progress',
-        function(e) {
+        e => {
           if (e.lengthComputable) {
-            var percentage = Math.round((e.loaded * 100) / e.total);
+            const percentage = Math.round((e.loaded * 100) / e.total);
             onProgress(percentage, e.loaded, e.total);
           }
         },
         false
       );
 
-      xhr.onreadystatechange = function() {
+      xhr.onreadystatechange = function readyStateChange() {
         if (xhr.readyState === 4) {
           if ((xhr.status >= 200 && xhr.status <= 200) || xhr.status === 304) {
             if (xhr.responseText !== '') {
@@ -26,7 +27,7 @@ const enableUploads = (onUploadComplete = () => {}) => {
         }
       };
 
-      var data = new FormData();
+      const data = new FormData();
       data.append('file', fileData);
       data.append('access', access);
       data.append('json', true);
@@ -38,25 +39,26 @@ const enableUploads = (onUploadComplete = () => {}) => {
     return uploadPromise;
   };
 
-  const displayProgress = (
-    name,
-    totalFiles,
-    currentFile,
-    percent,
-    loaded,
-    total
-  ) => {
+  const displayProgress = (name, totalFiles, currentFile, percent) => {
     const el = document.getElementById('status');
+
     el.style.backgroundSize = `${percent}%`;
+    if (percent < 100) {
+      if (totalFiles > 1) {
+        el.innerHTML = `[${currentFile}/${totalFiles}] Uploading ${name} ${percent}%`;
+      } else {
+        el.innerHTML = `Uploading ${name} ${percent}%`;
+      }
+    }
     if (totalFiles > 1) {
-      el.innerHTML = `[${currentFile}/${totalFiles}] Uploading ${name} ${percent}%`;
+      el.innerHTML = `[${currentFile}/${totalFiles}] Processing ${name} ${percent}%`;
     } else {
-      el.innerHTML = `Uploading ${name} ${percent}%`;
+      el.innerHTML = `Processing ${name} ${percent}%`;
     }
   };
 
-  const doUpload = (file, access, totalFiles, currentFile) => {
-    return uploadFile(file, access, (percent, uploaded, totalSize) => {
+  const doUpload = (file, access, totalFiles, currentFile) =>
+    uploadFile(file, access, (percent, uploaded, totalSize) => {
       displayProgress(
         file.name,
         totalFiles,
@@ -66,7 +68,6 @@ const enableUploads = (onUploadComplete = () => {}) => {
         totalSize
       );
     });
-  };
 
   const resetProgress = () => {
     const el = document.getElementById('status');
@@ -76,50 +77,52 @@ const enableUploads = (onUploadComplete = () => {}) => {
   };
 
   let doneTimeout;
-  document.addEventListener(
-    'drop',
-    function(event) {
-      console.log('drop');
-      event.preventDefault();
-      const files = event.files || event.dataTransfer.files;
+  const uploadFiles = files => {
+    // get current selected access level
+    const access = document
+      .getElementById('access_controls')
+      .querySelector('.primary').value;
 
-      // get current selected access level
-      const access = document
-        .getElementById('access_controls')
-        .querySelector('.primary').value;
-      console.log('found some files');
+    let promise;
 
-      let promise;
-
-      window.clearTimeout(doneTimeout);
-      resetProgress();
-      for (let i = 0; i < files.length; i += 1) {
-        const file = files[i];
-        if (promise) {
-          promise = promise.then(() => {
-            return doUpload(file, access, files.length, i + 1).then(
-              onUploadComplete
-            );
-          });
-        } else {
-          promise = doUpload(file, access, files.length, i + 1).then(
+    window.clearTimeout(doneTimeout);
+    resetProgress();
+    for (let i = 0; i < files.length; i += 1) {
+      const file = files[i];
+      if (promise) {
+        promise = promise.then(() => {
+          return doUpload(file, access, files.length, i + 1).then(
             onUploadComplete
           );
-        }
+        });
+      } else {
+        promise = doUpload(file, access, files.length, i + 1).then(
+          onUploadComplete
+        );
       }
+    }
 
-      promise.then(() => {
-        const el = document.getElementById('status');
-        el.classList.add('progress-hide');
-        doneTimeout = setTimeout(resetProgress, 5000);
-      });
+    return promise.then(() => {
+      const el = document.getElementById('status');
+      el.classList.add('progress-hide');
+      el.innerHTML = 'Done!';
+      doneTimeout = setTimeout(resetProgress, 5000);
+    });
+  };
+
+  document.addEventListener(
+    'drop',
+    event => {
+      event.preventDefault();
+      const files = event.files || event.dataTransfer.files;
+      uploadFiles(files);
     },
     false
   );
 
   document.addEventListener(
     'dragover',
-    function(event) {
+    event => {
       event.preventDefault();
     },
     false
@@ -127,7 +130,7 @@ const enableUploads = (onUploadComplete = () => {}) => {
 
   document.addEventListener(
     'dragleave',
-    function(event) {
+    event => {
       event.preventDefault();
     },
     false
@@ -138,7 +141,7 @@ const enableUploads = (onUploadComplete = () => {}) => {
     .getElementById('access_controls')
     .querySelectorAll('button')
     .forEach(el => {
-      el.addEventListener('click', event => {
+      el.addEventListener('click', () => {
         document
           .getElementById('access_controls')
           .querySelector('.primary')
@@ -146,4 +149,40 @@ const enableUploads = (onUploadComplete = () => {}) => {
         el.classList.add('primary');
       });
     });
+
+  // Add paste catcher
+  const pasteEl = document.getElementById('paste_box');
+  pasteEl.addEventListener('paste', ({ clipboardData }) => {
+    const { items } = clipboardData;
+    const files = [];
+    if (items) {
+      // Loop through all items, looking for any kind of image
+      for (let i = 0; i < items.length; i += 1) {
+        if (items[i].type.indexOf('image') !== -1) {
+          // TODO: test in chrome
+          const pasteFile = items[i].getAsFile();
+          if (pasteFile.name.startsWith('image.')) {
+            const now = new Date();
+            const name = `Image ${now.toDateString()} ${now.toLocaleTimeString()}.`;
+            files.push(
+              new File([pasteFile], pasteFile.name.replace('image.', name), {
+                type: pasteFile.type,
+              })
+            );
+          } else {
+            files.push(pasteFile);
+          }
+        }
+      }
+      if (files.length) {
+        uploadFiles(files);
+      }
+    } else {
+      console.log('No clipboard data found for pasting');
+    }
+    pasteEl.innerHTML = '';
+  });
+
+  document.body.addEventListener('click', () => pasteEl.focus());
+  pasteEl.focus();
 };
