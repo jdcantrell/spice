d05ce66f08106d3b0301a2174bbe5fe77c623ce8
jd <jd@goodrobot.net>
2024-05-05 21:45:33 -0700

    Initial attempt to update to latest versions of everything

--- readme.md
+++ readme.md
@@ -1,24 +1,48 @@
-#Spice
+# Spice
+
 Spice is a personal file sharing service. It aims to be a simple and
 clean alternative to cloud.app or dropbox for sharing files with
 friends.
 
-##Features:
-* Upload many files just by dragging and dropping them on to the page.
-* Upload files via cut and paste
-* Private, public, and limited (viewable by link only) sharing.
-* Syntax highlight code views
+## Todo:
 
-##Initial setup:
+- Poetry migration + dependency update
+- find right way for package to support cli
+- update flask to latest practice
+- update sqlalchemy to latest practice
+- see if we still need flask-login
+- update readme
+  - initial setup
+  - deploy steps
+- Fix image sizing
+
+## Maybe someday
+
+- Handle audio files nicely
+- Export endpoint (should zip all files with correct names
+  (when possible)
+- File sets so you can easily share multiple files
+- note field/custom key
+- See if we can tweak the short-id alphabet (remove `-` and `_`)
+
+## Features:
+
+- Upload many files just by dragging and dropping them on to the page.
+- Upload files via cut and paste
+- Private, public, and limited (viewable by link only) sharing.
+- Syntax highlight code views
+
+## Initial setup:
+
 1. Checkout this repo
 2. `pipenv install`
 3. Copy settings-example.cfg to settings.cfg and update values
 4. Export flask env vars:
-    export FLASK_APP=spice
-    export FLASK_ENV=development
-4. Initialize database: `flask init-db`
-5. Create a user: `flask create-user`
-6. Run the dev server: `flask run`
+   export FLASK_APP=spice
+   export FLASK_ENV=development
+5. Initialize database: `flask init-db`
+6. Create a user: `flask create-user`
+7. Run the dev server: `flask run`
 
 For production you will want to use something besides flask to run the server.
 This repo includes a spice.service file that uses uwsgi, you can update the
@@ -30,12 +54,3 @@ systemctl daemon-reload
 systemctl enable spice
 systemctl start spice
 ```
-
-##Todo:
-* Markdown rendering
-* Handle audio files nicely
-* Export endpoint (should zip all files with correct names
-  (when possible)
-* File sets so you can easily share multiple files
-* note field/custom key
-* See if we can tweak the short-id alphabet (remove `-` and `_`)
--- spice/__init__.py
+++ spice/__init__.py
@@ -1,53 +1,98 @@
+import click
 from flask import Flask
 from flask_login import LoginManager
 
+class DefaultConfig(object):
+    UPLOAD_FOLDER = './uploads'
+    DATABASE_FILE = './spice.sql'
+    CACHE_FOLDER = './cache'
 
-def create_app(test_config=None):
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    if test_config is None:
-        app.config.from_pyfile("../settings.cfg", silent=False)
-    else:
-        app.config.from_mapping(test_config)
+app.config.from_object(DefaultConfig)
+app.config.from_envvar("SPICE_SETTINGS", silent=True)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-    from . import database
+from . import database
 
-    database.init_app(app)
+database.init_app(app)
 
-    from . import models
+from . import models
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return database.get_db().query(models.User).get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return database.get_db().query(models.User).get(int(user_id))
 
-    from . import auth
+from . import auth
 
-    app.register_blueprint(auth.bp)
+app.register_blueprint(auth.bp)
 
-    from . import files
+from . import files
 
-    app.register_blueprint(files.bp)
-    app.add_url_rule("/<key>/<filename>", endpoint="files.view_raw")
-    app.add_url_rule("/<key>", endpoint="files.view")
+app.register_blueprint(files.bp)
+app.add_url_rule("/<key>/<filename>", endpoint="files.view_raw")
+app.add_url_rule("/<key>", endpoint="files.view")
 
-    from . import view
+from . import view
 
-    app.register_blueprint(view.bp)
+app.register_blueprint(view.bp)
 
-    from . import table
+from . import table
 
-    app.register_blueprint(table.bp)
-    app.add_url_rule("/", endpoint="table.index")
+app.register_blueprint(table.bp)
+app.add_url_rule("/", endpoint="table.index")
 
-    from . import log
+from . import log
 
-    app.register_blueprint(log.bp)
+app.register_blueprint(log.bp)
 
-    from . import tiles
+from . import tiles
 
-    app.register_blueprint(tiles.bp)
+app.register_blueprint(tiles.bp)
 
-    return app
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--name", prompt="Enter username")
+@click.option(
+    "--password", prompt="Enter password", hide_input=True, confirmation_prompt=True
+)
+def user_add(name, password):
+    from spice.models import User
+    from spice.database import db_session
+
+    user = User(name, password)
+
+    db_session.add(user)
+    db_session.commit()
+
+    print('User created: %r' % user.id)
+
+
+@cli.command()
+def create_db():
+    from spice import app
+    from spice.database import init_db
+    init_db()
+
+    print("Database init")
+
+@cli.command()
+def process():
+    from spice.models import File
+    from spice.database import db_session
+    from spice.handlers import get_handler_instance
+    files = db_session.query(File).order_by(File.id.desc()).all()
+
+    for record in files:
+        handler = get_handler_instance(record)
+        handler.process()
+        db_session.add(handler.record)
+
+        db_session.commit()
