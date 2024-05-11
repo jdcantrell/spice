a4fc35d165dd4ca9bd41172550e98776dfd10784
jd <jd@goodrobot.net>
2024-05-10 23:12:39 -0700

    A couple more cleanups

--- spice/cli.py
+++ spice/cli.py
@@ -5,39 +5,13 @@ import click
 def cli():
     pass
 
-
-@cli.command()
-@click.option("--name", prompt="Enter username")
-@click.option(
-    "--password", prompt="Enter password", hide_input=True, confirmation_prompt=True
-)
-def user_add(name, password):
-    from spice.models import User
-    from spice.database import db_session
-
-    user = User(name, password)
-
-    db_session.add(user)
-    db_session.commit()
-
-    print("User created: %r" % user.id)
-
-
-@cli.command()
-def create_db():
-    from spice.database import init_db
-
-    init_db()
-
-    print("Database init")
-
-
 @cli.command()
 def process():
     from spice.models import File
-    from spice.database import db_session
+    from spice.database import get_db
     from spice.handlers import get_handler_instance
 
+    db_session = get_db()
     files = db_session.query(File).order_by(File.id.desc()).all()
 
     for record in files:
